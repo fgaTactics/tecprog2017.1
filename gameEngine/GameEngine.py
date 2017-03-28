469ae4381293bd368f1cc4ef0d20b19688ed5876
Matheus Richard <matheusrichardt@gmail.com>
2017-03-28 13:20:12 -0300

    Refactor GameEngine variables

--- gameEngine/GameEngine.py
+++ gameEngine/GameEngine.py
@@ -1,6 +1,8 @@
 import pygame
 from gameEngine.GameCanvas import *
 from sys import exit
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 
 class GameEngine:
@@ -8,18 +10,18 @@ class GameEngine:
     def run(self):
         pygame.init()
 
-        while True:
+        canvas = GameCanvas(SCREEN_WIDTH, SCREEN_HEIGHT)
+        screen_name = "Start Game"
+        canvas.start_screen(screen_name)
 
-            canvas = GameCanvas(800, 600)
-            screen_name = "Start Game"
-            canvas.start_screen(screen_name)
+        while True:
 
             for event in pygame.event.get():
 
-                if(event.type == pygame.QUIT):
-                    exit()
-
-                else:
+                if(event.type != pygame.QUIT):
                     # do nothing
 
                     pygame.display.update()
+
+                else:
+                    exit()
