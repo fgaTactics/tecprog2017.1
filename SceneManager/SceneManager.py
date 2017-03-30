8c3f776cffdcf4638ab06af2097f2c323652efa2
Rodrigo <rodrigo.redcode@gmail.com>
2017-03-30 20:28:59 -0300

    Create SceneManager methods AddScene, Remove Scene, FindScene

--- SceneManager/SceneManager.py
+++ SceneManager/SceneManager.py
@@ -1,1 +1,22 @@
-class SceneManager
+from Scene import *
+
+class SceneManager(object):
+    
+    __gameSceneList = []
+    curScene = Scene()
+    
+    def AddScene(self, newScene):
+        gameSceneList.append(newScene)
+    
+    def RemoveScene(self, sceneName):
+        sceneToRemo = FindScene(sceneName)
+        gameSceneList.remove(sceneToRemove)
+        
+    def FindScene(sceneName):
+        sceneListLength = len(gameSceneList)
+        for scene in gameSceneList:
+            sceneIndex = gameSceneList.index(scene)
+            if(scene.name == sceneName):
+                return scene
+            elif(sceneIndex >= sceneListLength):
+                raise ValueError("This scene does not exists")
