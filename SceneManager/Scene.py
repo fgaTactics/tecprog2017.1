24d3785597b19f09e1790b0b49a39cba9d0a4e06
Rodrigo <rodrigo.redcode@gmail.com>
2017-03-30 21:06:11 -0300

    Modify scene atributes

--- SceneManager/Scene.py
+++ SceneManager/Scene.py
@@ -1,21 +1,8 @@
 class Scene(object):
+
+    def __init__(self, name = "DEFAULT", ID = 666):
+        self.name = name
+        self.ID = ID
+        
     
-    def __init__(self, sceneName=None):
-        self.sceneName = sceneName
-        
-    def set_name(self, stringValue):
-        stringValueLength = len(stringValue)
-        
-        if(isinstance(stringValue, str)):
-            if(stringValueLength < 15):
-                self._sceneName = stringValue
-            else:
-                raise ValueError("String value out of bounds", "Bound: 15")
-        else:
-            raise ValueError("String value is not of type string")
-        
-    sceneName = property(set_name)
-            
     
-teste = Scene("MINHASCENA")
-print(teste.sceneName)
