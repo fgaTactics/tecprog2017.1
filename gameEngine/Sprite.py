5aff111283a39016880ecf0365774ff728b2f31a
CaioNunes <caiofelipe147@gmail.com>
2017-03-31 21:08:14 -0300

    Create class to manage Sprites

---
+++ gameEngine/Sprite.py
@@ -0,0 +1,13 @@
+import pygame
+
+image_resources =  # Path of folder Assets on project
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, width, height, filename):
+        super(Sprite, self).__init__()
+        self.image = pygame.Surface((width, height))
+        self.rect = self.image.get_rect()
+
+        self.image = pygame.image.load(image_resources + filename)
+        self.image = pygame.transform.scale(self.image, (width, height))
