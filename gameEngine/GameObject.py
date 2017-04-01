cc63295f1d9d0d35900d79294d8e011fceba7fb8
josutk <josuetk63@gmail.com>
2017-03-31 22:28:54 -0300

    Create GameObject.

---
+++ gameEngine/GameObject.py
@@ -0,0 +1,26 @@
+
+
+class GameObject(SpriteController):
+    def __init__(self, color, width, height):
+        super().__init__(width,height)
+        self.width = width
+        self.height = height
+        self.color = color
+        #just use for test
+        pygame.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
+        self.rect = self.image.get_rect()
+
+
+    #movement sprit with mouse
+    def drag_and_drop_mouse_movement(self, playerGameObject, event):
+        if pygame.mouse.get_pressed()[0]:
+            mousePosicionX, mousePosicionY = event.pos
+            if playerGameObject.rect.collidepoint(mousePosicionX, mousePosicionY):
+                playerGameObject.rect.x = mousePosicionX - playerGameObject.rect.width / 2
+                playerGameObject.rect.y = mousePosicionY - playerGameObject.rect.height / 2
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels    
