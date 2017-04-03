3680f240d419ab0d359de3a710b27c591509862b
Rodrigo <rodrigo.redcode@gmail.com>
2017-04-03 20:25:50 -0300

    Add comments to the class and refactor method and variables names

--- sceneManager/SceneManager.py
+++ sceneManager/SceneManager.py
@@ -2,35 +2,67 @@ from Scene import *
 
 
 class SceneManager(object):
-    curScene = Scene()
-    gameSceneList = [curScene]
 
-    def AddScene(self, newScene):
-        self.gameSceneList.append(newScene)
+    # Scene that the game is showing to the player
+    current_scene = Scene()
 
-    def RemoveScene(self, sceneName):
-        sceneToRemove = self.FindScene(sceneName)
-        self.gameSceneList.remove(sceneToRemove)
-        if(sceneName == self.curScene.name):
-            self.curScene = Scene()
+    game_scene_list = [current_scene]
 
-    def LoadScene(self, sceneName):
-        sceneToLoad = self.FindScene(sceneName)
-        self.curScene = sceneToLoad
+    # Add a new scene to the game scene list
+    def add_scene(self, new_scene):
+        scene_to_compare_name = Scene()
+        scene_to_compare_name = self.find_scene(new_scene.name)
 
-    def LoadNextScene(self):
-        sceneListLength = len(self.gameSceneList)
-        nextSceneIndex = self.gameSceneList.index(self.curScene)+1
-        if(nextSceneIndex <= sceneListLength):
-            LoadScene(self.gameSceneList[nextSceneIndex].name)
+        # Check if exists a scene with the same name of the newScene
+        if(scene_to_compare_name.name == "DEFAULT"):
+            self.game_scene_list.append(new_scene)
         else:
-            raise ValueError("Next scene does not exists")
+            raise ValueError("This name of scene already exists")
 
-    def FindScene(self, sceneName):
-        sceneListLength = len(self.gameSceneList)
-        for scene in self.gameSceneList:
-            sceneIndex = self.gameSceneList.index(scene)
-            if(scene.name == sceneName):
+    # Remove a scene of the game
+    def remove_scene(self, scene_name):
+        scene_to_remove = Scene()
+        scene_to_remove = self.find_scene(scene_name)
+
+        """ Check if the scene to remove exists in the game
+            and if it is the currentScene """
+        if(scene_to_remove.name == "DEFAULT"):
+            self.game_scene_list.remove(scene_to_remove)
+            if(scene_name == self.current_scene.name):
+                self.current_scene = Scene()
+        else:
+            raise ValueError("This scene does not exists")
+
+    # Show a scene that exists in the game to the user
+    def LoadScene(self, scene_name):
+        scene_to_load = Scene()
+        scene_to_load = self.find_scene(scene_name)
+
+        # Checks if the scene exists in the game
+        if(scene_to_load.name != "DEFAULT"):
+            self.current_scene = scene_to_load
+        else:
+            raise ValueError("This scene does not exists")
+
+    # Show the next scene in the game scene list to the user
+    def load_next_scene(self):
+        scene_list_length = len(self.game_scene_list)
+        next_scene_index = self.game_scene_list.index(self.current_scene)+1
+
+        # Check if the current scene is the last scene of the game
+        if(next_scene_index <= scene_list_length):
+            load_scene(self.game_scene_list[next_scene_index].name)
+        else:
+            raise ValueError("The current scene is the last scene")
+
+    # Find a scene in the game scene list to use in others methods
+    def find_scene(self, scene_name):
+        scene_list_length = len(self.game_scene_list)
+
+        for scene in self.game_scene_list:
+            scene_index = self.game_scene_list.index(scene)
+
+            if(scene.name == scene_name):
                 return scene
-            elif(sceneIndex >= sceneListLength):
+            elif(scene_index >= scene_list_length):
                 raise ValueError("This scene does not exists")
