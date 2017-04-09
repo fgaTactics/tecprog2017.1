fb6d859c294817c2c16710225add752376cbb64b
Lucas <lucas465@hotmail.com>
2017-04-09 00:32:12 -0300

    Add position to GameObject

--- gameEngine/GameObject.py
+++ gameEngine/GameObject.py
@@ -4,13 +4,27 @@ from gameEngine.Sprite import *
 
 class GameObject:
 
-    def __init__(self, width, height, filename):
+    def __init__(self, x_position, y_position, width, height, filename):
         self.sprite = Sprite(filename)
         self.width = width
         self.height = height
-
+        self.set_x(x_position)
+        self.set_y(y_position)
+        
     def draw(self, groups):
         groups.add(self.sprite)
 
     def update(self):
         pass
+
+    def get_x(self):
+        return self.sprite.image.rect.x
+
+    def get_y(self):
+        return self.sprite.image.rect.y
+
+    def set_x(self, x_position):
+        self.sprite.rect.x = x_position
+
+    def set_y(self, y_position):
+        self.sprite.rect.y = y_position
