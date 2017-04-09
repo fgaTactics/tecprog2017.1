01b2cb274f5bc28834cc1ed323dcfd3a9ac1da60
Lucas <lucas465@hotmail.com>
2017-04-08 21:48:32 -0300

    Refactor Sprite Class
    Remove sprite transform by size, using now only real size of the loaded image.

--- gameEngine/Sprite.py
+++ gameEngine/Sprite.py
@@ -7,12 +7,9 @@ image_resources = "/home/josue/img/"
 
 
 class Sprite(pygame.sprite.Sprite):
-        def __init__(self, width, height, filename):
+        def __init__(self, filename):
 
                 super(Sprite, self).__init__()
-                self.image = pygame.Surface([width, height])
-                self.rect = self.image.get_rect()
                 self.image = pygame.image.load(image_resources +
                                                filename)
-                self.image = pygame.transform.scale(self.image, (width,
-                                                                 height))
+                self.rect = self.image.get_rect()
