a77c3ebe1343120799caccf1cc7233eac2d9e848
MatheusRich <matheusrichardt@gmail.com>
2017-04-10 11:35:50 -0300

    Create class ClassSelectionScene

--- main/main.py
+++ main/main.py
@@ -2,9 +2,12 @@
 from gameEngine.GameEngine import *
 from game.scenes.StartGameScene import *
 from game.scenes.ArmyPositioningScene import *
+from game.scenes.ClassSelectionScene import *
 
-scene = ArmyPositioningScene("example", 1)
+class ClassSelectionScene
 
+# scene = StartGameScene("example", 1)
+scene = ClassSelectionScene("example",1)
 gameEngine = GameEngine()
 
 gameEngine.add_scene(scene)
