124a61a78e08e2ec5103baa2d8738c6361b074dc
Lucas <lucas465@hotmail.com>
2017-04-11 13:32:04 -0300

    Refactor GameObject's positioning method

--- gameEngine/GameObject.py
+++ gameEngine/GameObject.py
@@ -11,21 +11,21 @@ class GameObject:
         self.set_x(x_position)
         self.set_y(y_position)
         self.sprite.resize(width, height)
-        
+
     def draw(self, groups):
         groups.add(self.sprite)
 
     def update(self):
         pass
 
-    def get_x(self):
-        return self.sprite.image.rect.x
-
-    def get_y(self):
-        return self.sprite.image.rect.y
-
     def set_x(self, x_position):
         self.sprite.rect.x = x_position
 
     def set_y(self, y_position):
         self.sprite.rect.y = y_position
+
+    def get_x(self):
+        return self.sprite.rect.x
+
+    def get_y(self):
+        return self.sprite.rect.y
