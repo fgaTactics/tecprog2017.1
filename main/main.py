a5f31f368a38e7288f64e798b76a647f2d527211
Rodrigo <rodrigo.redcode@gmail.com>
2017-04-13 14:37:22 -0300

    Fix main imports

--- main/main.py
+++ main/main.py
@@ -1,8 +1,8 @@
 
 from gameEngine.GameEngine import *
 from game.scenes.StartGameScene import *
-from game.scenes.GameBoardScene import *
 from game.scenes.ArmyPositioningScene import *
+
 scene = ArmyPositioningScene("example", 1)
 
 gameEngine = GameEngine()
