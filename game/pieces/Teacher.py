fa14949a39403b24c7ca053810354e95107df850
josutk <josuetk63@gmail.com>
2017-04-22 01:40:43 -0300

    Create class Teacher

---
+++ game/pieces/Teacher.py
@@ -0,0 +1,13 @@
+from game.pieces.BasicPiece import BasicPiece
+
+
+class Teacher(BasicPiece):
+
+    def __init__(self, health=0, attack=0, rangeAttack=0, defense=0,
+                 amount_of_moviment=0, penalty=0,
+                 hability="", description="", width=0,
+                 height=0, filename=""):
+        super().__init__(health, attack, rangeAttack, defense,
+                         amount_of_moviment, penalty,
+                         hability, description, width,
+                         height, filename)
