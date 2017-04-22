c1f7d64007225bcc22457e4767c733a5b207a38e
MatheuRich <matheusrichardt@gmail.com>
2017-04-22 20:49:42 -0300

    Apply constants and comments on ClassSceneSelect

--- game/scenes/ClassSelectionScene.py
+++ game/scenes/ClassSelectionScene.py
@@ -19,7 +19,6 @@ POSITION_Y_ELETRONIC = 100
 POSITION_Y_ENERGY = 400
 POSITION_Y_SOFTWARE = 400
 
-
 # Select the class wich the player wants to fight for
 class ClassSelectionScene(Scene):
 
@@ -31,7 +30,7 @@ class ClassSelectionScene(Scene):
                                           IMAGE_WIDTH,
                                           IMAGE_HEIGHT,
                                           "aerospace_class.png")
-
+        
         self.class_automotive = GameObject(POSITION_X_AUTOMOTIVE,
                                            POSITION_Y_AUTOMOTIVE,
                                            IMAGE_WIDTH,
@@ -85,3 +84,20 @@ class ClassSelectionScene(Scene):
         groups.add(self.class_eletronic.sprite)
         groups.add(self.class_energy.sprite)
         groups.add(self.class_software.sprite)
+        
+        mouse = Mouse()
+        
+        # Check where player click to select class
+        if (mouse.is_mouse_over(self.class_aerospace) and mouse.is_mouse_click(self.class_aerospace)):
+            print ("Você escolheu Aerospacial")
+        elif (mouse.is_mouse_over(self.class_automotive) and mouse.is_mouse_click(self.class_automotive)):
+            print ("Você escolheu Automotiva")
+        elif (mouse.is_mouse_over(self.class_eletronic) and mouse.is_mouse_click(self.class_eletronic)):
+            print ("Você escolheu Eletrônica")
+        elif (mouse.is_mouse_over(self.class_energy) and mouse.is_mouse_click(self.class_energy)):
+            print ("Você escolheu Energia")
+        elif (mouse.is_mouse_over(self.class_software) and mouse.is_mouse_click(self.class_software)):
+            print ("Você escolheu Software")
+        else:
+            # Nothing to do
+            pass
