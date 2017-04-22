76f8a93a4107dcf1fdbdbc6cd5104ada09f38ae8
MatheuRich <matheusrichardt@gmail.com>
2017-04-22 20:49:42 -0300

    Apply constants and comments on ClassSceneSelect

--- game/scenes/ClassSelectionScene.py
+++ game/scenes/ClassSelectionScene.py
@@ -6,6 +6,17 @@ from gameEngine.Mouse import *
 IMAGE_WIDTH = 100
 IMAGE_HEIGHT = 100
 
+POSITION_X_AEROSPACE = 50
+POSITION_X_AUTOMOTIVE = 350
+POSITION_X_ELETRONIC = 650
+POSITION_X_ENERGY = 200
+POSITION_X_SOFTWARE = 500
+
+POSITION_Y_AEROSPACE = 100
+POSITION_Y_AUTOMOTIVE = 100
+POSITION_Y_ELETRONIC = 100
+POSITION_Y_ENERGY = 400
+POSITION_Y_SOFTWARE = 400
 
 # Select the class which the player wants to fight for
 class ClassSelectionScene(Scene):
@@ -13,15 +24,34 @@ class ClassSelectionScene(Scene):
     def __init__(self, name="DEFAULT", ID=666):
         super().__init__(name, ID)
 
-        self.class_aerospace = GameObject(50, 100, IMAGE_WIDTH, IMAGE_HEIGHT,
+        self.class_aerospace = GameObject(POSITION_X_AEROSPACE,
+                                          POSITION_Y_AEROSPACE,
+                                          IMAGE_WIDTH,
+                                          IMAGE_HEIGHT,
                                           "aerospace_class.png")
-        self.class_automotive = GameObject(350, 100, IMAGE_WIDTH, IMAGE_HEIGHT,
+        
+        self.class_automotive = GameObject(POSITION_X_AUTOMOTIVE,
+                                           POSITION_Y_AUTOMOTIVE,
+                                           IMAGE_WIDTH,
+                                           IMAGE_HEIGHT,
                                            "automotive_class.png")
-        self.class_eletronic = GameObject(650, 100, IMAGE_WIDTH, IMAGE_HEIGHT,
+        
+        self.class_eletronic = GameObject(POSITION_X_ELETRONIC,
+                                          POSITION_Y_ELETRONIC,
+                                          IMAGE_WIDTH,
+                                          IMAGE_HEIGHT,
                                           "eletronic_class.png")
-        self.class_energy = GameObject(200, 400, IMAGE_WIDTH, IMAGE_HEIGHT,
+        
+        self.class_energy = GameObject(POSITION_X_ENERGY,
+                                       POSITION_Y_ENERGY,
+                                       IMAGE_WIDTH,
+                                       IMAGE_HEIGHT,
                                        "energy_class.png")
-        self.class_software = GameObject(500, 400, IMAGE_WIDTH, IMAGE_HEIGHT,
+        
+        self.class_software = GameObject(POSITION_X_SOFTWARE,
+                                         POSITION_Y_SOFTWARE,
+                                         IMAGE_WIDTH,
+                                         IMAGE_HEIGHT,
                                          "software_class.png")
 
     def update(self, event):
@@ -36,6 +66,7 @@ class ClassSelectionScene(Scene):
         
         mouse = Mouse()
         
+        # Check where player click to select class
         if (mouse.is_mouse_over(self.class_aerospace) and mouse.is_mouse_click(self.class_aerospace)):
             print ("Você escolheu Aerospacial")
         elif (mouse.is_mouse_over(self.class_automotive) and mouse.is_mouse_click(self.class_automotive)):
@@ -47,5 +78,6 @@ class ClassSelectionScene(Scene):
         elif (mouse.is_mouse_over(self.class_software) and mouse.is_mouse_click(self.class_software)):
             print ("Você escolheu Software")
         else:
+            # Nothing to do
             pass
     
