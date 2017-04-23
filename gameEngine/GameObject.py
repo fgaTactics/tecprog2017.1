903c010611b7a10daabea94db0681bf3684b1fbf
pablodiegoss <pablodiegoss@hotmail.com>
2017-04-23 20:57:45 -0300

    Refactor GameObject getters and setters for x and y
    Signed-off-by: ThalissonMelo<tallisonmelo@hotmail.com>

--- gameEngine/GameObject.py
+++ gameEngine/GameObject.py
@@ -14,26 +14,20 @@ class GameObject:
         self.set_y(y_position)
         self.sprite.resize(width, height)
 
-    def draw(self, groups):
+    def draw(self, screen, groups):
         groups.add(self.sprite)
 
     def update(self):
         pass
 
     def get_x(self):
-        return self.sprite.rect.x
+        return self.sprite.get_x()
 
     def get_y(self):
-        return self.sprite.rect.y
+        return self.sprite.get_y()
 
     def set_x(self, x_position):
-        self.sprite.rect.x = x_position
+        self.sprite.set_x(x_position)
 
     def set_y(self, y_position):
-        self.sprite.rect.y = y_position
-
-    def get_x(self):
-        return self.sprite.rect.x
-
-    def get_y(self):
-        return self.sprite.rect.y
+        self.sprite.set_y(y_position)
