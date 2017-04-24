e4ab5ffd9610c0129a816530c317596f41e4a496
pablodiegoss <pablodiegoss@hotmail.com>
2017-04-23 21:05:05 -0300

    refactor ArmyPositioningScene
    Signed-off-by: ThalissonMelo<tallisonmelo@hotmail.com>

--- game/scenes/ArmyPositioningScene.py
+++ game/scenes/ArmyPositioningScene.py
@@ -9,19 +9,18 @@ class ArmyPositioningScene(Scene):
 
         super().__init__(name, ID)
         self.game_board = GameBoard(60, 60)
-        self.left_piece_list = PieceList("software", 0, 0, 200, 800, "PieceMenu.png")
-        self.right_piece_list = PieceList("software", 1000, 0, 200, 800, "PieceMenu.png")
-        self.test_piece = DraggablePiece(0, 0, 50, 50, "MYP.png")
+        player1_class = "software"
+        player2_class = "eletronic"
+        self.left_piece_list = PieceList(player1_class, 0, 0, 200, 800, "PieceMenu.png")
+        self.right_piece_list = PieceList(player2_class, 1000, 0, 200, 800,
+                                          "PieceMenu.png")
 
     def draw(self, screen, groups):
         screen.fill((0, 0, 0))
         self.game_board.draw(screen)
-        groups.add(self.left_piece_list.sprite)
-        groups.add(self.right_piece_list.sprite)
-        groups.add(self.test_piece.sprite)
+        self.left_piece_list.draw(screen, groups)
+        self.right_piece_list.draw(screen, groups)
 
     def update(self, event):
-
         self.right_piece_list.update(event)
         self.left_piece_list.update(event)
-        self.test_piece.update(event)
