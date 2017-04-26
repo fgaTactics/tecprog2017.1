fd6f6c1aaf9fa6a57a4614c332f2833e7b276536
MatheusRich <matheusrichardt@gmail.com>
2017-04-25 22:27:24 -0300

    Fix missing arguments in BasicPiece and child classes

--- game/pieces/BasicPiece.py
+++ game/pieces/BasicPiece.py
@@ -1,15 +1,16 @@
 from gameEngine.GameObject import GameObject
 
-"""This class is a model for the game pieces"""
+# This class is a model for the game pieces
 
 
 class BasicPiece(GameObject):
 
-    def __init__(self, health=0, attack=0, rangeAttack=0, defense=0,
+    def __init__(self, x_position=0, y_position=0, health=0, attack=0, rangeAttack=0,
+                 defense=0,
                  amount_of_moviment=0, penalty=0,
                  hability="", description="", width=0,
                  height=0, filename=""):
-        super().__init__(width, height, filename)
+        super().__init__(x_position, y_position, width, height, filename)
         self.set_health(health)
         self.set_attack(attack)
         self.set_defense(defense)
