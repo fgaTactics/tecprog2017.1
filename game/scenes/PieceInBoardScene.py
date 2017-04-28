ae9ce6e3e1e28a564e456e8c4d306f068e703a90
josutk <josuetk63@gmail.com>
2017-04-28 11:34:48 -0300

    Put two Pieces in the board

--- game/scenes/PieceInBoardScene.py
+++ game/scenes/PieceInBoardScene.py
@@ -1,20 +1,18 @@
 from gameEngine.Scene import *
 from game.gameboard.GameBoard import *
 from game.pieces.Engineer import *
+from game.pieces.FreshMan import *
 
 
 class PieceInBoardScene(Scene):
-    engener = Engenner(health=0, attack=0, rangeAttack=0, defense=0,
-                       amount_of_moviment=0, penalty=0,
-                       hability="", description="",
-                       x_position=0, y_position=0,
-                       width=60, height=60, filename="MYP.png")
+    pieces = []
 
-    def __init__(self, name="DEFAULT", ID=0):
+    def __init__(self, name="DEFAULT", ID=0, pieces_list=None):
         super().__init__(name, ID)
         self.game_board = GameBoard(60, 60)
-
+        self.pieces = pieces_list
 
     def draw(self, screen, groups):
         self.game_board.draw(screen)
-        self.engener.draw(screen, groups)
+        for a in self.pieces:
+            a.draw(screen, groups)
--- main/main.py
+++ main/main.py
@@ -3,8 +3,25 @@ from game.scenes.StartGameScene import *
 from game.scenes.ArmyPositioningScene import *
 from game.scenes.ClassSelectionScene import *
 from game.scenes.PieceInBoardScene import *
+from game.pieces.Engineer import *
+from game.pieces.FreshMan import *
 
-scene = PieceInBoardScene("Start Menu", 0)
+
+engener = Engenner(health=0, attack=0, rangeAttack=0, defense=0,
+                   amount_of_moviment=0, penalty=0,
+                   hability="", description="",
+                   x_position=0, y_position=0,
+                   width=60, height=60, filename="MYP.png")
+
+engenver = FreshMan(health=0, attack=0, rangeAttack=0, defense=0,
+                    amount_of_moviment=0, penalty=0,
+                    hability="", description="",
+                    x_position=100, y_position=100,
+                    width=60, height=60, filename="MYP.png")
+pieces_list = []
+pieces_list.append(engener)
+pieces_list.append(engenver)
+scene = PieceInBoardScene("Start Menu", 0, pieces_list)
 
 
 gameEngine = GameEngine()
