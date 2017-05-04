8a85189241b6f771d8405a9b8dfd25b4cae15bc6
MatheusRich <matheusrichardt@gmail.com>
2017-05-04 15:48:59 -0300

    Add GameCanvas' set tests

--- tests/test_gameCanvas.py
+++ tests/test_gameCanvas.py
@@ -7,10 +7,18 @@ class GameCanvasTests(unittest.TestCase):
         self.canvas = GameCanvas(30, 30)
 
     def test_get_width(self):
-        self.assertEqual(self.canvas._GameCanvas__width, 30)
+        self.assertEqual(getattr(self.canvas, '_GameCanvas__width'), 30)
 
     def test_get_height(self):
-        self.assertEqual(self.canvas._GameCanvas__height, 30)
+        self.assertEqual(getattr(self.canvas, '_GameCanvas__height'), 30)
+
+    def test_set_width(self):
+        setattr(self.canvas, '_GameCanvas__width', 8)
+        self.assertEqual(getattr(self.canvas, '_GameCanvas__width'), 8)
+
+    def test_set_height(self):
+        setattr(self.canvas, '_GameCanvas__height', 10)
+        self.assertEqual(getattr(self.canvas, '_GameCanvas__height'), 10)
 
 if __name__ == '__main__':
         unittest.main()
