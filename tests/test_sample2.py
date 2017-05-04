3504f0e9db44f1531b4d95b17593d44d9491c82f
MatheusRich <matheusrichardt@gmail.com>
2017-05-04 09:53:44 -0300

    Add GameCanvas Tests

--- tests/test_sample2.py
+++ tests/test_sample2.py
@@ -1,6 +1,14 @@
-def func(x):
-    return x + 1
+import unittest
+from gameEngine.GameCanvas import *
 
+class GameCanvasTests(unittest.TestCase):
+    canvas = GameCanvas(30, 30)
 
-def test_answer():
-    assert func(3) == 4
+    def get_width_test(self):
+        self.assertEqual(canvas._GameCanvas__width, 30)
+        
+    def get_height_test(self):
+        self.assertEqual(canvas._GameCanvas__height, 30)
+
+if __name__ == '__main__':
+        unittest.main()
