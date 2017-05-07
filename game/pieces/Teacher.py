454af033afd24eff5b297aed425e41f0a07278ee
josutk <josuetk63@gmail.com>
2017-05-06 22:10:32 -0300

    Put comments and remove unnsual methods in class PieceInBoardScene

--- game/pieces/Teacher.py
+++ game/pieces/Teacher.py
@@ -5,9 +5,9 @@ class Teacher(BasicPiece):
 
     def __init__(self, health=0, attack=0, rangeAttack=0, defense=0,
                  amount_of_moviment=0, penalty=0,
-                 hability="", description="" ,x_position=0, y_position=0, width=0,
+                 hability="", description="", x_position=0, y_position=0, width=0,
                  height=0, filename=""):
         super().__init__(health, attack, rangeAttack, defense,
                          amount_of_moviment, penalty,
-                         hability, description,x_position, y_position, width,
+                         hability, description, x_position, y_position, width,
                          height, filename)
--- game/scenes/PieceInBoardScene.py
+++ game/scenes/PieceInBoardScene.py
@@ -5,38 +5,25 @@ from game.pieces.FreshMan import *
 from game.pieces.Teacher import *
 from game.ArmyPositionService import *
 
+"""This class show the pieces in the board"""
+
 
 class PieceInBoardScene(Scene):
-    
-       
+
+
     piecesInTheBoard = []
-    for i in range(0, 5):
-        freshMan = FreshMan(health=0, attack=0, rangeAttack=0, defense=0, 
-                            amount_of_moviment=0, 
-                           penalty=0, hability="", 
-                           description="hue", x_position=200, 
-                           y_position=50, width=60, 
-                           height=60, filename="MYP.png")
-        
-        piecesInTheBoard.append(freshMan)
-    for i in range(0, 10):
-        teacher = Teacher(health=0, attack=0, rangeAttack=0, defense=0, 
-                          amount_of_moviment=0, penalty=0, 
-                         hability="", description="hue1", x_position=150, 
-                           y_position=100, 
-                         width=60, height=60, filename="MYP.png")        
-        piecesInTheBoard.append(teacher)
-        
-    for i in range(0, 5):
-        engenner = Engenner(health=0, attack=0, rangeAttack=0, defense=0, 
-                            amount_of_moviment=0, 
-                           penalty=0, hability="", 
-                           description="hue2", x_position=1, 
-                           y_position=100, width=60, 
-                           height=60, filename="MYP.png")
-        piecesInTheBoard.append(engenner)
-        
-    
+
+
+    # create basic piece for test
+    teacher = Teacher(health=0, attack=0, rangeAttack=0, defense=0,
+                      amount_of_moviment=0, penalty=0,
+                      hability="", description="hue1", x_position=150,
+                      y_position=100,
+                      width=60, height=60, filename="MYP.png")
+    piecesInTheBoard.append(teacher)
+
+
+
     def __init__(self, name="DEFAULT", ID=0):
         super().__init__(name, ID)
         self.game_board = GameBoard(60, 60)
@@ -45,5 +32,4 @@ class PieceInBoardScene(Scene):
     def draw(self, screen, groups):
         self.game_board.draw(screen)
         for a in self.piecesInTheBoard:
-            a.draw(screen,groups)
-            
+            a.draw(screen, groups)
