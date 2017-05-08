39e055638b67c824371bff13a868ed2fd7ebff64
MatheusRich <matheusrichardt@gmail.com>
2017-05-08 09:44:33 -0300

    Add comments and contants

--- tests/test_gameObject.py
+++ tests/test_gameObject.py
@@ -1,24 +1,35 @@
 import unittest
 from gameEngine.GameObject import *
 
+# All the following numeric constants are pixels units
+X_POSITION = 30
+Y_POSITION = 40
+NEW_X = 10
+NEW_Y = 5
+WIDTH = 50
+HEIGHT = 50
+FILENAME = "start_button.png"
+
+# Tests for Game Object Class
+
 
 class GameObjectTests(unittest.TestCase):
     def setUp(self):
-        self.game_object = GameObject(30, 40, 50, 50, "start_button.png")
+        self.game_object = GameObject(X_POSITION, Y_POSITION, WIDTH, HEIGHT, FILENAME)
 
     def test_get_x(self):
-        self.assertEqual(self.game_object.get_x(), 30)
+        self.assertEqual(self.game_object.get_x(), X_POSITION)
 
     def test_get_y(self):
-        self.assertEqual(self.game_object.get_y(), 40)
+        self.assertEqual(self.game_object.get_y(), Y_POSITION)
 
     def test_set_x(self):
-        self.game_object.set_x(10)
-        self.assertEqual(self.game_object.get_x(), 10)
+        self.game_object.set_x(NEW_X)
+        self.assertEqual(self.game_object.get_x(), NEW_X)
 
     def test_set_y(self):
-        self.game_object.set_y(5)
-        self.assertEqual(self.game_object.get_y(), 5)
+        self.game_object.set_y(NEW_Y)
+        self.assertEqual(self.game_object.get_y(), NEW_Y)
 
 if __name__ == '__main__':
         unittest.main()
