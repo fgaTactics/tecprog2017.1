2e52d3be6be49a3ca254e6c5baf469895efb7c1b
MatheusRich <matheusrichardt@gmail.com>
2017-05-08 10:26:59 -0300

    Write logging to Move Piece Scene

--- game/scenes/MovePieceScene.py
+++ game/scenes/MovePieceScene.py
@@ -1,3 +1,4 @@
+import logging
 from gameEngine.Scene import *
 from game.gameboard.GameBoard import *
 from game.pieces.FreshMan import *
@@ -28,6 +29,8 @@ BOARD_TOP_LIMIT = 200
 class MovePieceScene(Scene):
 
     def __init__(self, name="DEFAULT", ID=0):
+        logging.info("Constructing Move Piece Scene")
+
         super().__init__(name, ID)
 
         self.game_board = GameBoard(BOARD_WIDTH, BOARD_HEIGHT)
@@ -45,49 +48,85 @@ class MovePieceScene(Scene):
                               AMOUNT_OF_MOVIMENT, PENALTY, HABILITY,
                               DESCRIPTION, WIDTH, HEIGHT, FILENAME)
 
+        logging.info("Move Piece Scene is ready")
+
 
     def update(self, event):
+        logging.debug("Beginning Move Piece scene's update method")
+
         mouse = Mouse()
 
         if (mouse.is_mouse_click(self.piece, event)):
+            logging.debug("Starting piece movement")
+
             y = self.down_movement(self.piece.get_y())
             self.piece.set_y(y)
         else:
             pass
 
+        logging.debug("Finishing Move Piece scene's update method")
+
 
     def right_movement(self, current_x):
+        logging.debug("Start right movement")
+
         if(current_x < (self.board_end_x - self.piece_step_size)):
+            logging.debug("Moving right")
+
             new_x_position = (current_x + self.piece_step_size)
             return new_x_position
         else:
             return current_x
 
+        logging.debug("Finishing right movement")
+
 
     def left_movement(self, current_x):
+        logging.debug("Start left movement")
+
         if(current_x > (self.board_start_x + self.piece_step_size)):
+            logging.debug("Moving left")
+
             new_x_position = (current_x - self.piece_step_size)
             return new_x_position
         else:
             return current_x
 
+        logging.debug("Finishing left movement")
+
 
     def up_movement(self, current_y):
+        logging.debug("Start up movement")
+
         if(current_y > (self.board_start_y + self.piece_step_size)):
+            logging.debug("Moving up")
+
             new_y_position = (current_y - self.piece_step_size)
             return new_y_position
         else:
             return current_y
 
+        logging.debug("Finishing up movement")
+
 
     def down_movement(self, current_y):
+        logging.debug("Start down movement")
+
         if(current_y < (self.board_end_y - self.piece_step_size)):
+            logging.debug("Moving down")
+
             new_y_position = (current_y + self.piece_step_size)
             return new_y_position
         else:
             return current_y
 
+        logging.debug("Finishing down movement")
+
 
     def draw(self, screen, groups):
+        logging.debug("Beginning Move Piece scene's draw method")
+
         self.game_board.draw(screen)
         self.piece.draw(screen, groups)
+
+        logging.debug("Finishing Move Piece scene's draw method")
