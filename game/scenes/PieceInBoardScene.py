c9ba48b93338339f9f76707f169c3ff6ece59399
josutk <josuetk63@gmail.com>
2017-05-13 11:48:47 -0300

    Create text turn player

--- game/scenes/PieceInBoardScene.py
+++ game/scenes/PieceInBoardScene.py
@@ -6,12 +6,16 @@ from game.pieces.FreshMan import *
 from game.pieces.Teacher import *
 from game.ArmyPositionService import *
 from game.gameboard.PieceMenu import *
+from game.PlayerService import *
+from gameEngine.GameText import *
 
 """This class show the pieces in the board"""
 
 # Constants to define board's width and height
 BOARD_WIDTH = 60
 BOARD_HEIGHT = 60
+TEXT_PLAYER_TURN_X = 500
+TEXT_PLAYER_TURN_Y = 100
 
 
 class PieceInBoardScene(Scene):
@@ -30,9 +34,12 @@ class PieceInBoardScene(Scene):
 
     teacher2 = Teacher(health=0, attack=0, rangeAttack=0, defense=0,
                        amount_of_moviment=0, penalty=0,
-                       hability="", description="Teacher2", x_position=150,
-                       y_position=170,
+                       hability="", description="Teacher2", x_position=350,
+                       y_position=500,
                        width=60, height=60, filename="teacher.jpg")
+
+    PlayerService.set_player1_pieces_list(teacher)
+    PlayerService.set_player2_pieces_list(teacher2)
 
     piecesInTheBoard.append(teacher)
     piecesInTheBoard.append(teacher2)
@@ -49,6 +56,12 @@ class PieceInBoardScene(Scene):
         for piece in self.piecesInTheBoard:
             piece.draw(screen, groups)
 
+        self.show_player_turn(1)
+
     def update(self, events):
         for piece in self.piecesInTheBoard:
             piece.update(events)
+
+    def show_player_turn(self, player_number):
+        GameText.print("Player 1 turn", TEXT_PLAYER_TURN_X,
+                       TEXT_PLAYER_TURN_Y)
