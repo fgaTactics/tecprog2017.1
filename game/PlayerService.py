2e6bd9ab6cda4ca3cbb31aadb19024590ee778fa
josutk <josuetk63@gmail.com>
2017-05-13 10:26:45 -0300

    Create players list piece in Player Service

--- game/PlayerService.py
+++ game/PlayerService.py
@@ -3,6 +3,8 @@
 class PlayerService:
 
     player_info = ["Player1 Class", "Player2 Class"]
+    player1_piece_list = []
+    player2_piece_list = []
 
     @classmethod
     def set_player(cls, player, player_number):
@@ -11,3 +13,19 @@ class PlayerService:
     @classmethod
     def get_player(cls, player_number):
         return cls.player_info[player_number]
+
+    @classmethod
+    def set_player1_pieces_list(cls, piece):
+        cls.player1_piece_list.append(piece)
+
+    @classmethod
+    def set_player2_pieces_list(cls, piece):
+        cls.player2_piece_list.append(piece)
+
+    @classmethod
+    def get_player1_list(cls, player_number):
+        return cls.player1_piece_list
+
+    @classmethod
+    def get_player(cls, player_number):
+        return cls.player2_piece_list
