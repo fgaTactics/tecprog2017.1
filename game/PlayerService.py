959da74094c613168df42213048de2235e3ab110
CaioNunes <caiofelipe147@gmail.com>
2017-05-15 23:51:28 -0300

    Merge PlayerService with devel

--- game/PlayerService.py
+++ game/PlayerService.py
@@ -3,8 +3,6 @@
 class PlayerService:
 
     player_info = ["Player1 Class", "Player2 Class"]
-    player1_piece_list = []
-    player2_piece_list = []
 
     @classmethod
     def set_player(cls, player, player_number):
@@ -13,23 +11,3 @@ class PlayerService:
     @classmethod
     def get_player(cls, player_number):
         return cls.player_info[player_number]
-
-    @classmethod
-    def set_player1_pieces_list(cls, piece):
-        cls.player1_piece_list.append(piece)
-
-    @classmethod
-    def set_player2_pieces_list(cls, piece):
-        cls.player2_piece_list.append(piece)
-
-    @classmethod
-    def get_player1_list(self):
-        return self.player1_piece_list
-
-    @classmethod
-    def get_player2_list(self):
-        return self.player2_piece_list
-
-    @classmethod
-    def get_player(cls, player_number):
-        return cls.player2_piece_list
