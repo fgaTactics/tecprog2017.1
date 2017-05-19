117953f0ae50188dc39c24fd1d0af55e1ec4e29d
ThalissonMelo <tallisonmelo@hotmail.com>
2017-05-19 15:32:38 -0300

    Add MovePieceScene to main

--- main/main.py
+++ main/main.py
@@ -5,6 +5,7 @@ from game.scenes.ClassSelectionScene import *
 from game.scenes.PieceInBoardScene import *
 from game.pieces.Engineer import *
 from game.pieces.FreshMan import *
+from game.scenes.MovePieceScene import *
 
 
 
@@ -12,14 +13,15 @@ scene = StartGameScene("Start Menu", 0)
 scene2 = ClassSelectionScene("Class Selection", 1)
 scene3 = ArmyPositioningScene("Army Positioning", 2)
 # scene4 = PieceInBoardScene("Start Menu", 0)
+scene5 = MovePieceScene("Moving", 4)
 
 gameEngine = GameEngine()
 gameEngine.add_scene(scene)
 gameEngine.add_scene(scene2)
 gameEngine.add_scene(scene3)
-# gameEngine.add_scene(scene4)
+gameEngine.add_scene(scene5)
 
 
 
-gameEngine.set_initial_scene("Start Menu")
+gameEngine.set_initial_scene("Moving")
 gameEngine.run()
