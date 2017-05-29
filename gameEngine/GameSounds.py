d4650754b51f8a90af40edc7b52dc297df2e2a79
CaioNunes <caiofelipe147@gmail.com>
2017-05-29 10:19:14 -0300

    Add log to GameSounds class

--- gameEngine/GameSounds.py
+++ gameEngine/GameSounds.py
@@ -1,4 +1,5 @@
 import pygame
+import logging
 
 # This class creates a controller to musics and sounds on game
 
@@ -12,9 +13,13 @@ class GameSounds:
         self.music_name = music_name
 
     def play_music(self):
-        assert self.music_name == "", "INVALID NAME"
+        assert self.music_name != "", "INVALID NAME"
+        
+        logging.info("Load music" + self.music_name)        
         pygame.mixer.init()
         pygame.mixer.music.load(MUSIC_PATH + self.music_name)
+        
+        logging.info("Play music" + self.music_name)
         pygame.mixer.music.play()
 
     def stop_music(self):
