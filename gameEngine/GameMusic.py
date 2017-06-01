6a5bb35ad198814cedbbe8f727be8ee0c17e2717
CaioNunes <caiofelipe147@gmail.com>
2017-06-01 12:33:48 -0300

    Create constants to define when music loops or not

--- gameEngine/GameMusic.py
+++ gameEngine/GameMusic.py
@@ -1,26 +1,27 @@
 import pygame
 import logging
 
-# This class creates a controller to musics and sounds on game
+# This class creates a controller to musics on game
 
 # Adding constant to musics path
-MUSIC_PATH = "../assets/sounds/"
-
+MUSIC_PATH = "../assets/music/"
+PLAY_ONCE = 0
+PLAY_LOOPING = -1
 
 class GameMusic:
 
     def __init__(self, music_name):
         self.music_name = music_name
+        pygame.mixer.init()
 
     def play_music(self):
         assert self.music_name != "", "INVALID NAME"
 
         logging.info("Load music" + self.music_name)
-        pygame.mixer.init()
         pygame.mixer.music.load(MUSIC_PATH + self.music_name)
 
         logging.info("Play music" + self.music_name)
-        pygame.mixer.music.play()
+        pygame.mixer.music.play(PLAY_LOOPING)
 
     def stop_music(self):
         pygame.mixer.music.stop()
