56f62942c90ee38a6ec3c47394a0c23b6aecbdca
ThalissonMelo <tallisonmelo@hotmail.com>
2017-06-25 19:33:24 -0300

    Prevent erros in the sound class

--- gameEngine/GameSounds.py
+++ gameEngine/GameSounds.py
@@ -1,10 +1,16 @@
 import pygame
 import logging
 
-# This class creates a controller sounds on game
+'''
+* This class manage the sound of buttons and clicks in all the game
+* Maked by students of software enginier on the dicipline Tecnicas de Programação
+* Students of Universidade de Brasília
+'''
 
 # Adding constant to sounds path
 SOUND_PATH = "../assets/sounds/"
+
+# This class creates a controller sounds on game
 
 
 class GameSounds:
@@ -12,9 +18,15 @@ class GameSounds:
     def __init__(self, sound_name):
         logging.info("Beggin of initialize method of game sound")
         self.sound_name = sound_name
+        assert sound_name is not None, "Invalide sound name"
         logging.info("End of initialize method of game sound")
 
     def play_sound(self):
-        logging.info("Playing sound button")
-        pygame.mixer.Sound(SOUND_PATH + self.sound_name).play()
-        logging.info("End of sound button")
+        try:
+            logging.info("Playing sound button")
+            # -- Play the sound --
+            pygame.mixer.Sound(SOUND_PATH + self.sound_name).play()
+            # -- End play the sound --
+            logging.info("End of sound button")
+        except:
+            logging.info("Fail to open the sound")
