e4857391f2944d57c02b8d30768d0864640dd27c
MartinsLucas <lucas465@hotmail.com>
2017-06-26 22:38:44 -0300

    Comment Mouse class

--- gameEngine/Mouse.py
+++ gameEngine/Mouse.py
@@ -6,10 +6,16 @@ import pygame
 class Mouse:
 
     def __init__(self):
+        # Array with mouse x and y positions
         self.position = pygame.mouse.get_pos()
+
+        # Array that indicate if some mouse button is pressed
         self.click = pygame.mouse.get_pressed()
 
+    # Verify if mouse cursor is over a certain object
     def is_mouse_over(self, element):
+
+        # Compare object coordinates with mouse position
         if((element.get_x() + element.get_width() > self.position[0] >
             element.get_x()) and (element.get_y() + element.get_height() >
                                   self.position[1] > element.get_y())):
@@ -17,17 +23,16 @@ class Mouse:
         else:
             return False
 
+    # Verify if the mouse cursor was over a certain object during a click event
     def is_mouse_click(self, element, event):
+
+        # Verify coincidence between mouse cursor and object position
         if(self.is_mouse_over(element)):
+
+            # Watch the mouse button release after a click
             if(event.type == pygame.MOUSEBUTTONUP):
                 return True
             else:
                 return False
         else:
             return False
-
-    def is_mouse_not_click(self, element):
-        if(self.is_mouse_over(element) and self.click[0]):
-            return False
-        else:
-            return True
